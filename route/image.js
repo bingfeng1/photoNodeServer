31ffb827d54e5841bb2258c4d8bff85533dfb57b
冰风 <wuxiaoming@baosight.com>
2019-08-09 15:13:50 +0800

    图片的后台数据获取

--- route/image.js
+++ route/image.js
@@ -2,20 +2,30 @@
 const Router = require('koa2-router');
 const router = new Router();
 const fs = require('fs')
+const {
+    INSERT,
+    SELECT,
+    UPDATE
+} = require('../mysql/sqlData')
 
 // 走马灯图片路由
 router.get('/carousel', async ctx => {
     let filePath = 'F:/learn/photoItem/photovue/src/assets/image';
     let fileList = await getImgPath(filePath)
-    
+
     ctx.body = fileList
 })
+    .get('/allImage', async ctx => {
+        let imgList = await SELECT([["image_url"],'product_image']).then(data=>data)
+        console.log(imgList)
+        ctx.body = imgList
+    })
 
 module.exports = router;
 
 // 获取图片地址（这个以后从数据库获取）
-const getImgPath = (filePath)=>{
-    return new Promise((resolve,reject)=>{
+const getImgPath = (filePath) => {
+    return new Promise((resolve, reject) => {
         fs.readdir(filePath, async (err, files) => {
             if (err) reject(new Error('读取文件失败' + err))
             resolve(files)
