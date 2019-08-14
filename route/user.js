1e60cd2e978c93c08395d2b5c267f1e47baba4d9
冰风 <wuxiaoming@baosight.com>
2019-08-14 15:40:25 +0800

    直接使用账号+排序用的id是有问题的 改为随即id

--- route/user.js
+++ route/user.js
@@ -4,6 +4,8 @@ const Router = require('koa2-router');
 const router = new Router();
 const dealSql = require('../mysql/sqlData') //封装的promise数据库方法
 const mysql = require('mysql')  //使用mysql.format 进行sql拼接
+// 生成随机码
+const nanoid = require('nanoid')
 
 // 走马灯图片路由
 router.get('/imgType', async ctx => {
@@ -19,6 +21,7 @@ router.get('/imgType', async ctx => {
     // 增加数据，并返回
     .post('/addImgType', async ctx => {
         let params = ctx.request.body;
+        params.id = nanoid();
         // 这里拼接sql语句
         let sql = mysql.format('INSERT INTO `userImgType` SET ? ', [params])
         await dealSql(sql).then((
@@ -30,10 +33,10 @@ router.get('/imgType', async ctx => {
         ctx.body = treeList
     })
     .delete('/deleteImgType', async ctx => {
-        let {account} = ctx.request.body;
-        let orderId = ctx.query['orderId[]'];
+        let { account } = ctx.request.body;
+        let id = ctx.query['id[]'];
         // 这里拼接sql语句
-        let sql = mysql.format('DELETE FROM `userImgType` WHERE account = ? and orderId IN (?) ', [account,orderId])
+        let sql = mysql.format('DELETE FROM `userImgType` WHERE id IN (?) ', [id])
         await dealSql(sql).then((
             { results }) => results)
 
@@ -43,13 +46,16 @@ router.get('/imgType', async ctx => {
         ctx.body = treeList
     })
     .put('/updateImgType', async ctx => {
-        let {account} = ctx.request.body;
-        let {orderId,typename} = ctx.query;
+        let { account } = ctx.request.body;
+        let { id, typename } = ctx.query;
         // 这里拼接sql语句
-        let sql = mysql.format('UPDATE `userImgType` SET ? WHERE account = ? and orderId = ? ', [{typename},account,orderId])
-        let result = await dealSql(sql).then((
+        let sql = mysql.format('UPDATE `userImgType` SET ? WHERE id = ? ', [{ typename }, id])
+        await dealSql(sql).then((
             { results }) => results)
-        ctx.body = result
+        sql = mysql.format('SELECT * FROM `userImgType` WHERE ? ORDER BY orderId', [{ account }])
+        let treeList = await dealSql(sql).then((
+            { results }) => results)
+        ctx.body = treeList
     })
 
 module.exports = router;
