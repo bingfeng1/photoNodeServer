1d9c5e52f491862f0be9577e1effaa469a9e59d0
冰风 <wuxiaoming@baosight.com>
2019-09-05 15:35:21 +0800

    增加了一些注釋

--- index.js
+++ index.js
@@ -8,24 +8,32 @@ const app = new Koa();
 const route = require('./route/allRouter')
 const serve = require('koa-static');
 
+// 这一块作为正式发布的错误处理部分，应该是类似邮件提示的东西
 const onError = err => {
     console.error(err)
 }
 
 const basePath = __dirname;
 
-app.use(logger())
-    .use(handleError(onError))
-    .use(bodyParser({multipart: true}))
+app.use(logger())   // 路由相关路径
+    .use(handleError(onError))  // 错误处理
+    // 这个multipart可能并没有用，因为文档中没有这一块东西。只是百度说可以解决文件上传的问题。后面可能用了其他方式解决了
+    .use(bodyParser({ multipart: true })) 
+    // 跨域处理，这个可以自己配置
     .use(cors())
+    // 上传图片的静态资源路径
     .use(serve(__dirname + '/uploads'))
+    // 上传头像的静态资源路径
     .use(serve(__dirname + '/portrait'))
-    .use(async (ctx,next)=>{
+    .use(async (ctx, next) => {
+        // 将所有路由分部绑入当前主入口文件的路径
         ctx.basePath = basePath;
         await next();
     })
+    // 所有自定义的路由放在最后，保证上面的中间件可以全部获取
     .use(route)
 
+    // 监听3000端口
     .listen(3000, '0.0.0.0', () => {
         console.log('成功启动服务')
     })
--- route/allRouter.js
+++ route/allRouter.js
@@ -5,8 +5,8 @@ const Login = require('./login')
 const Image = require('./image')
 const User = require('./user')
 
-router.use('/',Login)
-router.use('/image',Image)
-router.use('/user',User)
+router.use('/',Login)   // 登录注册相关路由
+router.use('/image',Image)  // 图片相关的路由
+router.use('/user',User)    //个人中心相关路由
 
-module.exports = router;
+module.exports = router;    // 在主程序中，引入，使用app.use(router)方式导入这个文件
